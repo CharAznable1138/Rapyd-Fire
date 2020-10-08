b5b4a2425701aeb4c10cf61a455edd56029c0db7
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-07 22:50:10 -0500

    added basic left/right movement to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float movementSpeed = 10;
+    [SerializeField] private float jumpForce = 10;
+    private float horizontalInput;
+    private bool isOnGround = false;
+
+    private Rigidbody2D rigidbody2D;
+
+    private void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(horizontalInput * movementSpeed , rigidbody2D.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
