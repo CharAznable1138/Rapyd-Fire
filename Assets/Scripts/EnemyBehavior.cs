970774f892aac4542fa414a6b4f4e10564f80d84
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-14 23:31:29 -0500

    implements basic enemy behavior
    + TODO: rework enemy bullet logic. currently, it only works if there's exactly 1 enemy on-screen

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10;
+
+    [SerializeField]
+    private float groundCheckDistance = 1;
+
+    [SerializeField]
+    private float cooldownTime = 0.5f;
+
+    [SerializeField]
+    private float firingRange = 10;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [SerializeField]
+    private LayerMask playerLayer;
+
+    private bool canShoot = true;
+    internal bool FacingRight { get; private set; }
+    internal bool AimingUp { get; private set; }
+
+    private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        FacingRight = true;
+        AimingUp = false;
+    }
+    private void Update()
+    {
+        Move();
+        if(canShoot && CanSeePlayer())
+        {
+            StartCoroutine("Shoot");
+        }
+    }
+    private bool IsOnGround()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = groundCheckDistance;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+
+    }
+    private bool CanSeePlayer()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction;
+        if(FacingRight)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.left;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, playerLayer);
+        if(hit.collider != null)
+        {
+            float distance = Math.Abs(hit.point.x - transform.position.x);
+            if(distance <= firingRange)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private bool IsOnEdge()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction;
+        if(FacingRight)
+        {
+            direction = Vector2.right - Vector2.up;
+        }
+        else
+        {
+            direction = Vector2.left - Vector2.up;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, groundCheckDistance, groundLayer);
+        if (hit.collider == null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private IEnumerator Shoot()
+    {
+        Instantiate(bulletPrefab, gameObject.transform);
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
+    }
+
+    private void Move()
+    {
+        if(!IsOnEdge())
+        {
+            if (FacingRight)
+            {
+                rigidbody2D.velocity = new Vector2(movementSpeed, rigidbody2D.velocity.y);
+            }
+            else
+            {
+                rigidbody2D.velocity = new Vector2(-movementSpeed, rigidbody2D.velocity.y);
+            }
+        }
+        else
+        {
+            Flip();
+        }
+    }
+    private void Flip()
+    {
+        if (FacingRight)
+        {
+            spriteRenderer.flipX = true;
+            FacingRight = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+            FacingRight = true;
+        }
+    }
+    private void Aim()
+    {
+        //TODO: Implement logic to allow enemy to shoot player if player is detected above enemy
+    }
+}
