664b6b926b3f7a1fbe10b5e5cfa860a7cff2b51a
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-21 21:32:39 -0500

    implements basic aiming reticle

---
+++ Assets/Scripts/AimingReticle.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingReticle : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    [SerializeField]
+    private Vector3 aimingRightVector;
+
+    [SerializeField]
+    private Vector3 aimingLeftVector;
+
+    [SerializeField]
+    private Vector3 aimingUpVector;
+
+    [SerializeField]
+    private Vector3 aimingDownVector;
+
+    private void Start()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if(playerController.AimingUp)
+        {
+            transform.localPosition = aimingUpVector;
+        }
+        else if(playerController.AimingDown)
+        {
+            transform.localPosition = aimingDownVector;
+        }
+        else
+        {
+            if(playerController.FacingRight)
+            {
+                transform.localPosition = aimingRightVector;
+            }
+            else
+            {
+                transform.localPosition = aimingLeftVector;
+            }
+        }
+    }
+}
