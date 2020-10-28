f4dc0f63a848b5a878b33fa11160b6761ce48a94
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 16:55:01 -0500

    splits button manager into main button manager and menu button manager

---
+++ Assets/Scripts/MenuButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
