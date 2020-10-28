551d0228f3aa619273890b7ccbf6c689c7d010d4
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 18:42:45 -0500

    adds playerspawner script

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject canvas;
+
+    [SerializeField]
+    private GameObject mainCamera;
+
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+
+    internal void SpawnPlayer()
+    {
+        Instantiate(player, gameObject.transform);
+        Instantiate(canvas);
+        Instantiate(mainCamera);
+    }
+}
