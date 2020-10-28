5a476225d0ffa75a3c0609e7848411c310fd6db9
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 17:06:21 -0500

    adds all necessary methods to menu button manager + hooks up methods to all buttons on main menu scene

--- Assets/Scripts/MenuButtonManager.cs
+++ Assets/Scripts/MenuButtonManager.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainPanel;
+
+    [SerializeField]
+    private GameObject instructionsPanel;
+
+    [SerializeField]
+    private GameObject creditsPanel;
+    private void Start()
+    {
+        ShowMain();
+    }
+
     public void QuitGame()
     {
         #if UNITY_EDITOR
@@ -16,5 +29,21 @@ public class MenuButtonManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ShowMain()
+    {
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+    public void ShowInstructions()
+    {
+        mainPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+    }
+    public void ShowCredits()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
     }
 }
