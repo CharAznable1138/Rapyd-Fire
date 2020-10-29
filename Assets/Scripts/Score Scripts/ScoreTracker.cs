64ec5357bcb1e7e5826b119b14557a6742b80cdb
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 22:18:11 -0500

    refactors scoretracker script to prevent score from dropping below 0

--- Assets/Scripts/Score Scripts/ScoreTracker.cs
+++ Assets/Scripts/Score Scripts/ScoreTracker.cs
@@ -7,4 +7,11 @@ public class ScoreTracker : Singleton<MonoBehaviour>
 {
     private float score;
     internal float Score { get { return score; } set { score = value; } }
+    private void Update()
+    {
+        if(score < 0)
+        {
+            score = 0;
+        }
+    }
 }
