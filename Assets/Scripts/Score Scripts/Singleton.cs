874b9ecd6f2e2627071cad5a71a7cf1b265b4730
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 22:14:03 -0500

    adds singleton script

---
+++ Assets/Scripts/Score Scripts/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Generic that inherits from MonoBehavior ensures that we can use the Unity API.
+// Where T is MonoBehaviour (or inherits from MonoBehaviour) ensures T to Singleton<T>.
+public class Singleton<T> : MonoBehaviour
+    where T : MonoBehaviour
+{
+    static public T Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
