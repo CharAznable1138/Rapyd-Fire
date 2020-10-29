dbd22ffcef4f907be40a7cc599161a2eaf95019f
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 22:14:33 -0500

    adds scoredisplay script

---
+++ Assets/Scripts/Score Scripts/ScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    private GameObject scoreTrackerObject;
+    private ScoreTracker scoreTrackerScript;
+    private TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreTrackerObject = GameObject.FindGameObjectWithTag("Score Tracker");
+        scoreTrackerScript = scoreTrackerObject.GetComponent<ScoreTracker>();
+        scoreText = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = $"Score: {scoreTrackerScript.Score}";
+    }
+}
