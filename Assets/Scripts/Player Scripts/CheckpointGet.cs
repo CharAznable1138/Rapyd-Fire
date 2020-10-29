b35a55caca44ab3a865830be60c5639b82b7191d
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 19:44:17 -0500

    adds checkpointget script

---
+++ Assets/Scripts/Player Scripts/CheckpointGet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointGet : MonoBehaviour
+{
+    [SerializeField]
+    private float textDisplayTime = 2;
+
+    private GameObject spawnPoint;
+    internal bool IsClaimed { get; private set; }
+    private void Start()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("Spawnpoint");
+        IsClaimed = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            spawnPoint.transform.position = gameObject.transform.position;
+            StartCoroutine("Claim");
+        }
+    }
+    private IEnumerator Claim()
+    {
+        IsClaimed = true;
+        yield return new WaitForSeconds(textDisplayTime);
+        IsClaimed = false;
+        gameObject.SetActive(false);
+    }
+
+}
