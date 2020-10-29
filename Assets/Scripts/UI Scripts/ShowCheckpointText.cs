cb5e80cc679efd85ea307af2d7d8e8ff58363414
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 19:44:02 -0500

    adds showcheckpointtext script

---
+++ Assets/Scripts/UI Scripts/ShowCheckpointText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCheckpointText : MonoBehaviour
+{
+    private GameObject checkpoint;
+    private CheckpointGet checkpointGet;
+
+    [SerializeField]
+    private GameObject checkpointText;
+
+    private void Start()
+    {
+        checkpoint = GameObject.FindGameObjectWithTag("Checkpoint");
+        if (checkpoint != null)
+        {
+            checkpointGet = checkpoint.GetComponent<CheckpointGet>();
+        }
+    }
+    private void Update()
+    {
+        if (checkpoint != null)
+        {
+            if (checkpointGet.IsClaimed)
+            {
+                checkpointText.SetActive(true);
+            }
+            else
+            {
+                checkpointText.SetActive(false);
+            }
+        }
+    }
+}
