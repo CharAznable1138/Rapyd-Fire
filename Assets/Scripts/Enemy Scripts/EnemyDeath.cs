bb709b5eb969c1c490a0822c52c57a527386c128
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 22:15:41 -0500

    refactors enemy death script to grant points

--- Assets/Scripts/Enemy Scripts/EnemyDeath.cs
+++ Assets/Scripts/Enemy Scripts/EnemyDeath.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+    [SerializeField]
+    private float enemyPoints = 10;
+
+    private GameObject scoreTrackerObject;
+    private ScoreTracker scoreTrackerScript;
+    private bool enemyIsDead = false;
+
+    private void Start()
+    {
+        scoreTrackerObject = GameObject.FindGameObjectWithTag("Score Tracker");
+        scoreTrackerScript = scoreTrackerObject.GetComponent<ScoreTracker>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player Bullet"))
         {
+            if (!enemyIsDead)
+            {
+                scoreTrackerScript.Score += enemyPoints;
+                enemyIsDead = true;
+            }
             Destroy(gameObject);
         }
     }
