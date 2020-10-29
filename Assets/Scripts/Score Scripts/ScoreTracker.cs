d98891be0ad5cbc1c24b7590dc260efc73218a57
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-28 22:14:21 -0500

    adds scoretracker script

---
+++ Assets/Scripts/Score Scripts/ScoreTracker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : Singleton<MonoBehaviour>
+{
+    private float score;
+    internal float Score { get { return score; } set { score = value; } }
+}
