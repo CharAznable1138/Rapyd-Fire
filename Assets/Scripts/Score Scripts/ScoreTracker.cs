c8b26811997dbbaf1340b1dfb99b81be8b342a77
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-03 20:34:42 -0600

    refactors scoretracker logic to add modifiable initial score

--- Assets/Scripts/Score Scripts/ScoreTracker.cs
+++ Assets/Scripts/Score Scripts/ScoreTracker.cs
@@ -5,6 +5,14 @@ using UnityEngine.UI;
 
 public class ScoreTracker : Singleton<MonoBehaviour>
 {
+    [SerializeField]
+    private float starterScore = 0;
+
     private float score;
     internal float Score { get { return score; } set { score = value; } }
+
+    private void Start()
+    {
+        score = starterScore;
+    }
 }
