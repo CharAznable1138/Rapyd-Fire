f73b652cee6aaa48717a3402b4e1ccd3a9b594a6
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-10 19:18:04 -0600

    adds logic that knows when score changes & by how much
    TODO: replace Debug.Log with logic that causes score text to spawn

--- Assets/Scripts/Score Scripts/ScoreTracker.cs
+++ Assets/Scripts/Score Scripts/ScoreTracker.cs
@@ -9,10 +9,27 @@ public class ScoreTracker : Singleton<MonoBehaviour>
     private float starterScore = 0;
 
     private float score;
-    internal float Score { get { return score; } set { score = value; } }
+    private float scoreChange;
+    internal float Score 
+    { 
+        get { return score; } 
+        set 
+        {
+            scoreChange = value - score;
+            score = value;
+        } 
+    }
 
     private void Start()
     {
         score = starterScore;
+    }
+    private void Update()
+    {
+        if(score != 0 && scoreChange != 0)
+        {
+            Debug.Log($"Score just changed by {scoreChange} points.");
+            scoreChange = 0;
+        }
     }
 }
