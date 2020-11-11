5967a470d12b2297eab1179853068285121abf8c
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-10 19:41:54 -0600

    adds score text behavior (makes score text move slowly up and then disappear)
    TODO: make text move down if it's a decrement instead of an increment

---
+++ Assets/Scripts/ScoreTextBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTextBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float maximumHeight;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up *moveSpeed * Time.deltaTime);
+        if(transform.position.y > maximumHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
