ee133b18607a8647b10fe52f9f1865a2904a508f
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-10 21:06:10 -0600

    changes scoretext behavior so that score text moves down if score is decremented

--- Assets/Scripts/Score Scripts/ScoreTextBehavior.cs
+++ Assets/Scripts/Score Scripts/ScoreTextBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreTextBehavior : MonoBehaviour
@@ -8,12 +9,29 @@ public class ScoreTextBehavior : MonoBehaviour
     private float maximumHeight;
 
     [SerializeField]
+    private float minimumHeight;
+
+    [SerializeField]
     private float moveSpeed;
+
+    private TMP_Text text;
+
+    private void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
 
     private void Update()
     {
-        transform.Translate(Vector3.up *moveSpeed * Time.deltaTime);
-        if(transform.position.y > maximumHeight)
+        if (text.text.Contains("+"))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else if (text.text.Contains("-"))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+        if(transform.position.y > maximumHeight || transform.position.y < minimumHeight)
         {
             Destroy(gameObject);
         }
