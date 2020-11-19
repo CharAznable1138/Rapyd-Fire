c9cc68a41419558dde81c21cbe5b9f68e375c60a
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-19 00:09:11 -0600

    adds tooltips and summaries to all custom fields and methods in ShowShieldGetText

--- Assets/Scripts/UI Scripts/ShowShieldGetText.cs
+++ Assets/Scripts/UI Scripts/ShowShieldGetText.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ShowShieldGetText : MonoBehaviour
 {
+    [Tooltip("The Player's game object.")]
     private GameObject player;
+    [Tooltip("The PlayerController script attached to the Player's game object.")]
     private PlayerController playerController;
 
     [SerializeField]
+    [Tooltip("The UI object that displays the Shield Get text.")]
     private GameObject shieldGetText;
 
     private void Update()
@@ -28,6 +31,10 @@ public class ShowShieldGetText : MonoBehaviour
             shieldGetText.SetActive(false);
         }
     }
+    /// <summary>
+    /// Check if the Player's game object exists, and assign the PlayerController if so.
+    /// </summary>
+    /// <returns></returns>
     private bool PlayerExists()
     {
         player = GameObject.FindGameObjectWithTag("Player");
