651232d7329c925f96a80759803683164f7a0674
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-19 00:02:25 -0600

    adds tooltips and summaries to all custom fields and methods in ShieldTimerBehavior

--- Assets/Scripts/UI Scripts/ShieldTimerBehavior.cs
+++ Assets/Scripts/UI Scripts/ShieldTimerBehavior.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 
 public class ShieldTimerBehavior : MonoBehaviour
 {
+    [Tooltip("The amount of time at which to start the shield timer. (Float)")]
     private double startingTime;
+    [Tooltip("The amuont of time left before the shield timer expires. (Float)")]
     private double timeRemaining;
+    [Tooltip("The amount of time that has passed since this object was instantiated. (Float)")]
     private double timePassed;
+    [Tooltip("The amount of time left before the shield timer expires. (String)")]
     private string timeRemainingString;
+    [Tooltip("The Player's game object.")]
     private GameObject player;
+    [Tooltip("The PlayerController script attached to the Player's game object.")]
     private PlayerController playerController;
+    [Tooltip("The TextMeshPro Text component attached to the this game object.")]
     private TMP_Text timerText;
 
     [SerializeField]
+    [Tooltip("The amount of decimal places to which to round up remaining time. (Integer)")]
     private int decimalPlaces = 2;
 
     private void Start()
@@ -48,7 +56,10 @@ public class ShieldTimerBehavior : MonoBehaviour
             }
         }
     }
-
+    /// <summary>
+    /// Check if the Player's game object exists, and assign the PlayerController if it does.
+    /// </summary>
+    /// <returns></returns>
     private bool PlayerExists()
     {
         player = GameObject.FindGameObjectWithTag("Player");
