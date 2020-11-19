2c31e0571a55098d71a108979152af791661f821
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-18 23:51:03 -0600

    adds tooltips and summaries to all custom fields and methods in TextManager

--- Assets/Scripts/UI Scripts/HintTextManager.cs
+++ Assets/Scripts/UI Scripts/HintTextManager.cs
@@ -6,45 +6,63 @@ using UnityEngine;
 public class HintTextManager : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("The text to display for movement hints. (String)")]
     private string movementHintText = "Movement hint text will be here.";
     [SerializeField]
+    [Tooltip("The amount of time to wait before displaying movement hints. (Float)")]
     private float movementHintDelay = 1;
 
     [SerializeField]
+    [Tooltip("The text to display for shooting hints. (String)")]
     private string shootHintText = "Shooting hint text will be here.";
     [SerializeField]
+    [Tooltip("The amount of time to wait before displaying shooting hints. (Float)")]
     private float shootHintDelay = 10;
 
     [SerializeField]
+    [Tooltip("The text to display for jumping hints. (String)")]
     private string jumpHintText = "Jumping hint text will be here.";
     [SerializeField]
+    [Tooltip("The amount of time to wait before displaying jumping hints. (Float)")]
     private float jumpHintDelay = 5;
 
     [SerializeField]
+    [Tooltip("The text to display for movement-locking hints. (String)")]
     private string lockHintText = "Movement locking hint text will be here.";
     [SerializeField]
+    [Tooltip("The amount of time to wait before displaying movement-locking hints. (Float)")]
     private float lockHintDelay = 30;
 
     [SerializeField]
+    [Tooltip("The text to display for aiming hints. (String)")]
     private string aimHintText = "Aiming hint text will be here.";
     [SerializeField]
+    [Tooltip("The amount of time to wait before displaying aiming hints. (Float)")]
     private float aimHintDelay = 20;
 
     [SerializeField]
+    [Tooltip("The amount of time to show hints. (Float)")]
     private float hintTime = 5;
 
     [SerializeField]
+    [Tooltip("The UI object that displays hint text.")]
     private GameObject hintTextObject;
 
+    [Tooltip("The TextMeshPro Text component attached to the Hint Text UI object.")]
     private TMP_Text hintTextComponent;
 
+    [Tooltip("The Player's game object.")]
     private GameObject player;
+    [Tooltip("The PlayerController component attached to the Player's game object.")]
     private PlayerController playerController;
 
+    [Tooltip("True = Show Hint coroutine is currently running, False = Show Hint coroutine is not currently running. (Bool)")]
     private bool showHintCoroutineIsRunning;
 
+    [Tooltip("List of hints to show to the Player.")]
     private List<Hint> hints = new List<Hint>();
 
+    [Tooltip("List of hints the Player has already seen, and does not need to see again.")]
     private List<Hint> noShowHints = new List<Hint>();
 
     private void Start()
@@ -66,6 +84,10 @@ public class HintTextManager : MonoBehaviour
             StartCoroutine(ShowHint());
         }
     }
+    /// <summary>
+    /// Show the player a hint, and add said hint to the list of hints the player has already seen.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator ShowHint()
     {
         showHintCoroutineIsRunning = true;
@@ -116,6 +138,10 @@ public class HintTextManager : MonoBehaviour
         showHintCoroutineIsRunning = false;
         yield break;
     }
+    /// <summary>
+    /// Check if the Player's game object exists. True = yes, False = no.
+    /// </summary>
+    /// <returns></returns>
     private bool PlayerExists()
     {
         player = GameObject.FindGameObjectWithTag("Player");
