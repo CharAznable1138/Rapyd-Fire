9593da17f1f3bdd562928d86b762bbc8bf407f3c
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-19 00:07:24 -0600

    adds tooltips and summaries to all custom fields and methods in ShowGameOverScreen

--- Assets/Scripts/UI Scripts/ShowGameOverScreen.cs
+++ Assets/Scripts/UI Scripts/ShowGameOverScreen.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ShowGameOverScreen : MonoBehaviour
 {
+    [Tooltip("The Player's game object.")]
     private GameObject player;
 
     [SerializeField]
+    [Tooltip("The UI panel that displays if the Player is defeated.")]
     private GameObject gameOverPanel;
 
     [SerializeField]
+    [Tooltip("The UI panel that displays if the Player completes the level.")]
     private GameObject victoryPanel;
 
     private void Update()
@@ -30,6 +33,10 @@ public class ShowGameOverScreen : MonoBehaviour
             }
         }
     }
+    /// <summary>
+    /// Check if the Player's game object exists. True = yes, False = no.
+    /// </summary>
+    /// <returns></returns>
     private bool PlayerExists()
     {
         player = GameObject.FindGameObjectWithTag("Player");
