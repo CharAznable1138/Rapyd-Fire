7211c29515d7cc6cf77dbe1c354f7a55a8ffaebf
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-19 00:20:57 -0600

    adds tooltips and summaries to all custom fields and methods in PlayerSpawner

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("The Player's game object.")]
     private GameObject player;
 
     [SerializeField]
+    [Tooltip("The Canvas containing all UI objects in the scene.")]
     private GameObject canvas;
 
     [SerializeField]
+    [Tooltip("The UI panel which displays an array of relevant information the player unless the player is defeated or the level is completed.")]
     private GameObject HUD;
 
     private void Start()
     {
         SpawnPlayer();
     }
-
+    /// <summary>
+    /// Create a new Player game object and a new HUD UI object.
+    /// </summary>
     internal void SpawnPlayer()
     {
         Instantiate(player, gameObject.transform.position, player.transform.rotation);
