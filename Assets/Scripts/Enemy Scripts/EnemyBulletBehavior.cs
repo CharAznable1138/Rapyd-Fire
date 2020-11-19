327e639058157464bca24c29fd824a1dbb724e7f
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-18 22:34:51 -0600

    adds tooltips and summaries to all custom fields and methods in EnemyBulletBehavior

--- Assets/Scripts/Enemy Scripts/EnemyBulletBehavior.cs
+++ Assets/Scripts/Enemy Scripts/EnemyBulletBehavior.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class EnemyBulletBehavior : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("The speed at which enemy bullets travel.")]
     private float bulletSpeed = 50;
 
     [SerializeField]
+    [Tooltip("Time to wait until enemy bullet destroys itself.")]
     private float selfDestructCountdown = 1;
 
+    [Tooltip("Rigidbody2D component attached to the enemy bullet prefab.")]
     private Rigidbody2D rigidbody2D;
 
+    [Tooltip("EnemyBehavior script attached to the enemy prefab.")]
     private EnemyBehavior enemyBehavior;
 
+    [Tooltip("Direction in which enemy bullet will move.")]
     private Vector2 movementVector;
 
     private void Start()
@@ -26,6 +31,9 @@ public class EnemyBulletBehavior : MonoBehaviour
 
         StartCoroutine("SelfDestruct");
     }
+    /// <summary>
+    /// Creates an enemy bullet and fires it in a specified direction.
+    /// </summary>
     private void Shoot()
     {
         if (enemyBehavior.AimingUp)
@@ -45,7 +53,10 @@ public class EnemyBulletBehavior : MonoBehaviour
         }
         rigidbody2D.velocity = movementVector;
     }
-
+    /// <summary>
+    /// Makes enemy bullet destroy itself.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator SelfDestruct()
     {
         yield return new WaitForSeconds(selfDestructCountdown);
