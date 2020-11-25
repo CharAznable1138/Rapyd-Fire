f890a104b44298c7be6a57e111663c8cfa5d1dd3
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-24 18:46:56 -0600

    extracted code from Start and OnTriggerEnter2D into custom methods with summaries in CheckpointGet

--- Assets/Scripts/Player Scripts/CheckpointGet.cs
+++ Assets/Scripts/Player Scripts/CheckpointGet.cs
@@ -20,17 +20,42 @@ public class CheckpointGet : MonoBehaviour
     internal float TextDisplayTime { get { return textDisplayTime; } }
     private void Start()
     {
-        spawnPoint = GameObject.FindGameObjectWithTag("Spawnpoint");
+        FindSpawnpointGameObject();
+        SetIsClaimedToFalse();
+    }
+    /// <summary>
+    /// Initialize "IsClaimed" boolean as false.
+    /// </summary>
+    private void SetIsClaimedToFalse()
+    {
         IsClaimed = false;
     }
+
+    /// <summary>
+    /// Find the game object tagged "Spawnpoint".
+    /// </summary>
+    private void FindSpawnpointGameObject()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("Spawnpoint");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        StartClaimCoroutineIfTouchingPlayer(collision);
+    }
+    /// <summary>
+    /// Check if the checkpoint is touching the player. If so, start the Claim coroutine.
+    /// </summary>
+    /// <param name="collision">The collider that just touched the checkpoint.</param>
+    private void StartClaimCoroutineIfTouchingPlayer(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
         {
             spawnPoint.transform.position = gameObject.transform.position;
             StartCoroutine("Claim");
         }
     }
+
     /// <summary>
     /// Display Checkpoint Get text for a specified amount of time, and let other scripts know that the checkpoint has been claimed.
     /// </summary>
