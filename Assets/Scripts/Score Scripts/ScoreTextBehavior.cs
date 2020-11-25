3fa7ea0c379405bc400c77801f56e501125c736f
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-11-24 20:41:34 -0600

    extracts code from Start and Update into custom methods with summaries in ScoreTextBehavior

--- Assets/Scripts/Score Scripts/ScoreTextBehavior.cs
+++ Assets/Scripts/Score Scripts/ScoreTextBehavior.cs
@@ -22,10 +22,36 @@ public class ScoreTextBehavior : MonoBehaviour
 
     private void Start()
     {
+        FindTMPTextComponent();
+    }
+    /// <summary>
+    /// Find this score text instance's TMP Text component.
+    /// </summary>
+    private void FindTMPTextComponent()
+    {
         text = GetComponent<TMP_Text>();
     }
 
     private void Update()
+    {
+        MoveText();
+        DestroyText();
+    }
+    /// <summary>
+    /// If the text has exceeded maximum or minimum height, destroy it.
+    /// </summary>
+    private void DestroyText()
+    {
+        if (transform.position.y > maximumHeight || transform.position.y < minimumHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Move the text upward if points were gained, or downward if points were lost.
+    /// </summary>
+    private void MoveText()
     {
         if (text.text.Contains("+"))
         {
@@ -34,10 +60,6 @@ public class ScoreTextBehavior : MonoBehaviour
         else if (text.text.Contains("-"))
         {
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
-        }
-        if(transform.position.y > maximumHeight || transform.position.y < minimumHeight)
-        {
-            Destroy(gameObject);
         }
     }
 }
