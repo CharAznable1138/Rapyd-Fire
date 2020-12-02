0c9bc10e30a155f1c0986dbb22e84d90234ad461
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-12-02 16:35:57 -0600

    adds PowerupSpawner and EnemySpawner scripts + reorganizes all Spawner scripts into new Spawner Scripts folder

---
+++ Assets/Scripts/Spawner Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The prefab containing all the enemies in the level.")]
+    private GameObject enemySet;
+
+    /// <summary>
+    /// Respawn all the enemies in the level.
+    /// </summary>
+    internal void SpawnEnemies()
+    {
+        Instantiate(enemySet, enemySet.transform.position, enemySet.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/Spawner Scripts/PowerupSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The prefab containing all the powerups in the level.")]
+    private GameObject powerupSet;
+
+    /// <summary>
+    /// Respawn all the powerups in the level.
+    /// </summary>
+    internal void SpawnPowerups()
+    {
+        Instantiate(powerupSet, powerupSet.transform.position, powerupSet.transform.rotation);
+    }
+}
