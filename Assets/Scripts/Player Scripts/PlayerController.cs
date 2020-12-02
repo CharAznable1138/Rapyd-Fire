11eb4572850501178f9b77d108b7e1d8555a892c
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-12-01 19:04:07 -0600

    removed logic from PlayerController that allowed IsOnGround to return true if y-velocity is 0
    it came to my attention that this code, intended as a hacky fix to a previous physics bug, may uninentionally allow players to jump infinitely many times if they press space at just the right moment mid-jump

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -184,7 +184,7 @@ public class PlayerController : MonoBehaviour
         float distance = groundCheckDistance;
 
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
-        if(hit.collider != null || rigidbody2D.velocity.y == 0)
+        if(hit.collider != null)
         {
             return true;
         }
