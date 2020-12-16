f303ccc76b4c7d24c9440392be993a45cefc2619
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-12-16 17:31:57 -0600

    renames "CollectibleCollect" to "PowerupCollect"

--- Assets/Scripts/Player Scripts/PowerupCollect.cs
+++ Assets/Scripts/Player Scripts/PowerupCollect.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectibleCollect : MonoBehaviour
+public class PowerupCollect : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         SelfDestructOnContactWithPlayer(collision);
     }
     /// <summary>
-    /// Check if this collectible just touched the player. If so, destroy this collectible.
+    /// Check if this powerup just touched the player. If so, destroy this powerup.
     /// </summary>
     /// <param name="collision">The collider that just touched this collectible.</param>
     private void SelfDestructOnContactWithPlayer(Collision2D collision)
