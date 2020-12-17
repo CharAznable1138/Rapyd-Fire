a748cc913d5edca064dd7f4deb4de183723f6d85
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-12-16 19:29:37 -0600

    implements jetpack sound + updates credits accordingly

---
+++ Assets/Scripts/Enemy Scripts/PlayJetpackSoundWhenVisible.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayJetpackSoundWhenVisible : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        GetAudioSource();
+    }
+    private void GetAudioSource()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnBecameVisible()
+    {
+        PlayJetpackSound();
+    }
+    private void OnBecameInvisible()
+    {
+        StopJetpackSound();
+    }
+    private void PlayJetpackSound()
+    {
+        audioSource.Play();
+    }
+    private void StopJetpackSound()
+    {
+        audioSource.Stop();
+    }
+}
