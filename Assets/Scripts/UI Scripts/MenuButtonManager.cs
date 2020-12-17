ff93a82dcd54075147cbf95a76a1077a98ed5e04
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-12-16 22:41:50 -0600

    adds logic to MenuButtonManager to allow 2-part credits

--- Assets/Scripts/UI Scripts/MenuButtonManager.cs
+++ Assets/Scripts/UI Scripts/MenuButtonManager.cs
@@ -16,6 +16,22 @@ public class MenuButtonManager : MonoBehaviour
     [SerializeField]
     [Tooltip("The UI panel containing credits to external assets.")]
     private GameObject creditsPanel;
+
+    [SerializeField]
+    [Tooltip("The button which can be used to access Credits Part 2 from Credits Part 1.")]
+    private GameObject nextCreditsButton;
+
+    [SerializeField]
+    [Tooltip("The button which can be used to access Credits Part 1 from Credits Part 2.")]
+    private GameObject previousCreditsButon;
+
+    [SerializeField]
+    [Tooltip("Part 1 of the Credits.")]
+    private GameObject creditsPart1;
+
+    [SerializeField]
+    [Tooltip("Part 2 of the Credits.")]
+    private GameObject creditsPart2;
     private void Start()
     {
         ShowMain();
@@ -62,5 +78,26 @@ public class MenuButtonManager : MonoBehaviour
     {
         mainPanel.SetActive(false);
         creditsPanel.SetActive(true);
+        CreditsPart1();
+    }
+    /// <summary>
+    /// Show Part 1 of the Credits text, and the Next button to access Part 2.
+    /// </summary>
+    public void CreditsPart1()
+    {
+        nextCreditsButton.SetActive(true);
+        creditsPart1.SetActive(true);
+        creditsPart2.SetActive(false);
+        previousCreditsButon.SetActive(false);
+    }
+    /// <summary>
+    /// Show Part 2 of the Credits text, and the Previous button to access Part 1.
+    /// </summary>
+    public void CreditsPart2()
+    {
+        nextCreditsButton.SetActive(false);
+        creditsPart1.SetActive(false);
+        creditsPart2.SetActive(true);
+        previousCreditsButon.SetActive(true);
     }
 }
